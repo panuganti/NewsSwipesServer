441b4f077b0873996aca5486be1dc293ac02b830
rapanuga <rapanuga@microsoft.com>
2016-03-22 12:18:33 -0700

    Add NewsSwipesServer Project

---
+++ NewsSwipesServer/Controllers/feedController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using NewsSwipesServer.Models;
+
+namespace NewsSwipesServer.Controllers
+{
+    public class feedController : ApiController
+    {
+        // GET api/feed
+        public Article Get()
+        {
+            return new Article() { Url = "Url", Heading = "Heading", ImageUrl = "ImageUrl"};
+        }
+
+        // GET api/feed/5
+        public string Get(int id)
+        {
+            return "feed";
+        }
+
+        // POST api/feed
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/feed/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/feed/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ NewsSwipesServer/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsSwipesServer.Models
+{
+    public class Article
+    {
+        public string Url { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Heading { get; set; }
+    }
+}
