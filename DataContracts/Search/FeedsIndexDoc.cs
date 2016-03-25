a0f80ccc68e89e41e41a9689abd3b755e6462e11
Rajkiran Panuganti <rajkiran.panuganti@gmail.com>
2016-03-24 17:47:04 -0700

    Commit deployed changes

--- DataContracts/Search/FeedsIndexDoc.cs
+++ DataContracts/Search/FeedsIndexDoc.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 namespace DataContracts.Search
 {
     [DataContract]
-    public class Feed : SearchDoc
+    public class FeedsIndexDoc : SearchDoc
     {
         [DataMember]
         public string Title { get; set; }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,8 +1,5 @@
 ﻿using GoogleDatastore;
 using Search;
-using Microsoft.Azure.Search;
-using Microsoft.Azure.Search.Models;
-using DataContracts;
 using DataContracts.Search;
 using System;
 
@@ -12,28 +9,11 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            TestSearch();
-        }
-
-        static void TestUploadToIndex()
-        {
-            var feed = new Search.Feed();
-            var article = new Article();
-            article.CardStyle = CardType.Default.ToString();
-            feed.UploadDocument(article, "2").Wait();
-        }
-
-        static void TestSearch()
-        {
-            var feed = new Search.Feed();
-            var result = feed.Search("*").Result;
-            Console.WriteLine("",result.Results.Count);
-            Console.ReadKey();
         }
 
         static void TestIndexSearch()
         {
-            var image = new Image();
+            var image = new ImagesIndexDoc();
             image.DateAdded = DateTime.Now;
             image.Id = Guid.NewGuid().ToString();
             image.SourceUrl = "http://i.ndtvimg.com/i/2016-02/arun-jaitley-budget_650x400_51456737432.jpg";
